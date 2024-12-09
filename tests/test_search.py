fbd7d019cdf572bfe38b321f639a76089ea07a19
Andrey Tkachev <tkachyov03@inbox.ru>
2024-12-10 01:36:00 +0300

    uncommented items

--- tests/test_search.py
+++ tests/test_search.py
@@ -5,9 +5,9 @@ from pages.search_page import SearchPage
 
 class TestSearch:
     @pytest.mark.parametrize("item_name", [
-        # {"name": "MOVE S", "color": "черный + Оливье"},
-        # {"name": "VENDI 11", "color": "серо-зеленый металлик"},
-        # {"name": "POUCH", "color": "черный"},
+        {"name": "MOVE S", "color": "черный + Оливье"},
+        {"name": "VENDI 11", "color": "серо-зеленый металлик"},
+        {"name": "POUCH", "color": "черный"},
         {"name": "Топ TUSA", "color": "Розово-красный"}
     ])
     def test_search_item_success(self, browser, item_name):
