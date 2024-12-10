eb180983cc611a8c67f132e3cf61bda208a490be
4veg0s <tkachyov03@inbox.ru>
2024-12-10 11:35:08 +0300

    fixed check of products in cart in edit cart

--- pages/cart_edit_page.py
+++ pages/cart_edit_page.py
@@ -20,9 +20,14 @@ class CartEditPage(BasePage):
     _CART_TABLE_PRODUCT_NAME = (By.XPATH, "//div[@class='h6']")
     _CART_TABLE_PRODUCT_COLOR = (By.XPATH, "//div[contains(@class,'color-name')]")
 
+
     _CLEAR_CART_BUTTON = (By.XPATH, "//button[@value='cart/clean']")
 
     def __init__(self, driver):
+        self.cart_card_names = None
+        self.cart_card_colors = None
+        self.card_names = None
+        self.card_colors = None
         self.catalog_cards_slice = None
         self.add_to_cart_buttons_slice = None
         self.cart_cards = None
@@ -30,12 +35,17 @@ class CartEditPage(BasePage):
 
     def init_catalog_cards(self):
         cards = self.find_elements(self._CARD_LOCATOR)
+        self.card_names = [element.text for element in self.find_elements(self._CARD_TITLE_NAME_LOCATOR)]
+        self.card_colors = [element.text for element in self.find_elements(self._CARD_SUBTITLE_COLOR_LOCATOR)]
         add_to_cart_buttons = self.find_elements(self._ADD_TO_CART_BUTTON)
         self.catalog_cards_slice = cards[:3]  # срез для 3-х первых товаров из каталога
         self.add_to_cart_buttons_slice = add_to_cart_buttons[:3]  # срез для 3-х первых товаров из каталога
 
     def init_cart_cards(self):
         self.cart_cards = self.find_elements(self._CART_TABLE_ELEMENT)
+        self.cart_card_names = [element.text for element in self.find_elements(self._CART_TABLE_PRODUCT_NAME)]
+        self.cart_card_colors = [element.text for element in self.find_elements(self._CART_TABLE_PRODUCT_COLOR)]
+        assert len(self.cart_cards) == len(self.catalog_cards_slice)
 
     def fill_cart(self):
         self.driver.execute_script("window.scrollTo(0, 500)")
@@ -60,18 +70,11 @@ class CartEditPage(BasePage):
         self.driver.get(self.base_url + self._CART_URL)
 
     def is_item_added_successfully(self):   # FIXME: не работает
-        """Проверка, что все элеменгты были добавлены в корзину"""
+        """Проверка, что все элементы были добавлены в корзину"""
         return all(
-            [(str.lower(self.find_nested_element(self.catalog_cards_slice[i], self._CART_TABLE_PRODUCT_NAME).text) ==
-              str.lower(self.find_nested_element(self.cart_cards[i], self._CARD_TITLE_NAME_LOCATOR).text)
+            [(str.lower(self.card_names[i]) ==
+              str.lower(self.cart_card_names[i]))
               and
-              str.lower(self.find_nested_element(self.catalog_cards_slice[i], self._CART_TABLE_PRODUCT_COLOR).text) ==
-              str.lower(self.find_nested_element(self.cart_cards[i], self._CARD_SUBTITLE_COLOR_LOCATOR).text)
-              ) for i in range(len(self.catalog_cards_slice))])
-
-    def are_all_items_found_truly_black(self):
-        target_color = 'черный'
-        cards = self.find_elements(self._CARD_LOCATOR)
-        return all(
-            [target_color in str.lower(self.get_nested_element_text(card, self._CARD_SUBTITLE_COLOR_LOCATOR)) for card
-             in cards])
+             (str.lower(self.card_colors[i]) ==
+              str.lower(self.cart_card_colors[i]))
+             for i in range(len(self.catalog_cards_slice))])
